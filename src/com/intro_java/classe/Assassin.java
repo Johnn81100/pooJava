f7c0aedb7c49bf83704e4c993d5c6d472d91f87e
simoncauhape <138566911+simoncauhape@users.noreply.github.com>
2024-02-12 16:17:31 +0100

    ajout classe Assassin exercice 2

--- src/com/intro_java/classe/Assassin.java
+++ src/com/intro_java/classe/Assassin.java
@@ -1,2 +1,29 @@
-package com.intro_java.classe;public class Assassin {
+package com.intro_java.classe;
+
+public class Assassin extends Personnage{
+
+ private int bonusAttaque;
+
+    public int getBonusAttaque() {
+        return bonusAttaque;
+    }
+
+    public void setBonusAttaque(int bonusAttaque) {
+        this.bonusAttaque = bonusAttaque;
+    }
+
+    public Assassin(String nom, int vie, int defense, int attaque, int bonusAttaque){
+     super(nom, vie,defense,attaque);
+     this.bonusAttaque = bonusAttaque;
+ }
+
+    public int attaquer(Personnage personnage){
+        long random = Math.round(Math.random()*100);
+        if(random % 20 == 0) {
+            return personnage.getVie() - ((this.getAttaque() + this.getBonusAttaque()) - personnage.getDefense());
+        }else {
+            return personnage.getVie() - (this.getAttaque() - personnage.getDefense());
+        }
+    }
+
 }
