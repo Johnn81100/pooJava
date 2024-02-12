3416f9153534cd0aa91c1d2d74f796485732f133
simoncauhape <138566911+simoncauhape@users.noreply.github.com>
2024-02-12 16:17:58 +0100

    ajout classe Guerrier exercice 3

--- src/com/intro_java/classe/Guerrier.java
+++ src/com/intro_java/classe/Guerrier.java
@@ -1,2 +1,27 @@
-package com.intro_java.classe;public class Guerrier {
+package com.intro_java.classe;
+
+public class Guerrier extends Personnage {
+    private int bonusDefense;
+
+    public Guerrier(String nom, int vie, int defense, int attaque, int bonusDefense){
+        super(nom, vie,defense,attaque);
+        this.bonusDefense = bonusDefense;
+    }
+
+    public int getBonusDefense() {
+        return bonusDefense;
+    }
+
+    public void setBonusDefense(int bonusDefense) {
+        this.bonusDefense = bonusDefense;
+    }
+    public int attaquer(Personnage personnage){
+        long random = Math.round(Math.random()*100);
+        if(random % 20 == 0) {
+            this.setVie(this.getVie()+ getBonusDefense());
+        }
+            return personnage.getVie() - (this.getAttaque() - personnage.getDefense());
+
+    }
+
 }
